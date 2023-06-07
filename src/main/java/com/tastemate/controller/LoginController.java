cbd79cabb513408f73632ba58683002af2d80328
ksb96 <ksebin96@gmail.com>
2023-06-07 16:59:33 +0900

    로그인 / 핸들러 / 매퍼 삭제(김세빈)

--- src/main/java/com/tastemate/controller/LoginController.java
+++ src/main/java/com/tastemate/controller/LoginController.java
@@ -1,54 +1,32 @@
 package com.tastemate.controller;
 
-import com.tastemate.domain.member.MemberVO;
-import com.tastemate.form.MemberLoginForm;
-import com.tastemate.service.login.LoginService;
-import java.io.IOException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import com.tastemate.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/login")
-@RequiredArgsConstructor
-@Slf4j
 public class LoginController {
 
-  private final LoginService loginService;
+    private final MemberService service;
 
-  @GetMapping
-  public String loginForm(Model model) {
-    MemberLoginForm loginForm = new MemberLoginForm();
-    model.addAttribute("member", loginForm);
-    return "member/login/loginform";
-  }
-
-  @PostMapping
-  public String login(@ModelAttribute(name = "member") MemberLoginForm loginForm,
-      HttpServletRequest request, @RequestParam(defaultValue = "/") String redirectURL) throws IOException {
-    if (loginService.loginCheck(loginForm.getId(), loginForm.getPassword())) {
-      MemberVO findMember = loginService.getMemberById(loginForm.getId());
-      HttpSession session = request.getSession();
-      session.setAttribute("userIdx", findMember.getUserIdx());
+    @Autowired
+    public LoginController(MemberService service) {
+        this.service = service;
     }
-    return "redirect:" + redirectURL;
-  }
 
-  @GetMapping("/logout")
-  public String logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    HttpSession session = request.getSession(false);
-    session.invalidate();
-    return "login_home";
-  }
+    	// 로그인 폼으로 이동
+        @GetMapping("/login/loginform")
+        public String loginform() {
+//		System.out.println("로그인폼");
+            return "member/login/loginform";
+        }
+
+        // 일반 회원 회원가입 폼으로 이동
+        @GetMapping("/member/registerForm")
+        public String registerMember() {
+            return "member/login/registerForm";
+        }
+
+
 }
