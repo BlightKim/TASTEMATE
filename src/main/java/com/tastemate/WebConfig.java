9f64c84292eaaf4fae7e9dd0ad3a0ba66896f916
bazzi <chsh620@gmail.com>
2023-06-16 12:22:51 +0900

    6/16 상현 합치기

--- src/main/java/com/tastemate/WebConfig.java
+++ src/main/java/com/tastemate/WebConfig.java
@@ -31,7 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
                     "/member/mbti", "/member/selectFood", "/member/list", "/member/simpleregister",
                     "/member/userJoin", "/member/findId", "/member/findIdByEmail", "/store/list", "/store/main",
                     "/member/resetPassword", "/member/reset", "/member/checkId", "/tastemate", "/member/tastemate",
-                    "/member/resetSuccess", "/member/mailConfirm", "/member/sweetalert2.min.css");
+                    "/member/resetSuccess", "/member/mailConfirm", "/member/sweetalert2.min.css", "/bookmark/get");
 
   }
   private String resourcePath = "/member/**";
