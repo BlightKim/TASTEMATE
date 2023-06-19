a10c38f0fac35a170a3bb2bce6e5ae2c77be834c
ksb96 <ksebin96@gmail.com>
2023-06-20 01:32:46 +0900

    상현 수정 반영

--- src/main/java/com/tastemate/service/MemberService.java
+++ src/main/java/com/tastemate/service/MemberService.java
@@ -382,18 +382,7 @@ public class MemberService {
         String msgg = "";
         msgg += "<div style='margin:100px;'>";
         msgg += "<h1> 안녕하세요</h1>";
-        msgg += "<h1> 통합 취업 정보 포탈 TASTEMATE 입니다</h1>";
-        msgg += "<br>";
-        msgg += "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>";
-        msgg += "<br>";
-        msgg += "<p>항상 당신의 꿈을 응원합니다. 감사합니다!<p>";
-        msgg += "<br>";
-        msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg += "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
-        msgg += "<div style='font-size:130%'>";
-        msgg += "CODE : <strong>";
-        msgg += ePw + "</strong><div><br/> "; // 메일에 인증번호 넣기
-        msgg += "</div>";
+        msgg += "<h1> TASTEMATE 입니다</h1>";
         message.setText(msgg, "utf-8", "html");// 내용, charset 타입, subtype
         // 보내는 사람의 이메일 주소, 보내는 사람 이름
         message.setFrom(new InternetAddress("chsh0620@naver.com", "TASTEMATE_Admin"));// 보내는 사람
