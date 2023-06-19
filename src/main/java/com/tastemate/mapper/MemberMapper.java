1f32dee16fa94c2cba489e11074f4cf14f4b1525
ksb96 <ksebin96@gmail.com>
2023-06-19 15:22:04 +0900

    최종본

--- src/main/java/com/tastemate/mapper/MemberMapper.java
+++ src/main/java/com/tastemate/mapper/MemberMapper.java
@@ -1,47 +1,47 @@
 package com.tastemate.mapper;
 
 import com.tastemate.domain.MemberVO;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
 
 @Mapper
 public interface MemberMapper {
 
-        @Autowired
-        public List<MemberVO> member_getList();
-
-        @Autowired
-        public String member_checkId();
-
-        public int user_join(MemberVO vo);
-
-        public Integer checkId(String userId);
-
-        public MemberVO loginId(String userId);
-
-        public int userModify(MemberVO vo);
-
-        public int userStatus(MemberVO vo);
-
-        public MemberVO findId(String userEmail);
-
-        public Integer check(String userId, String userPwd);
-
-        public MemberVO userGet(String userId, String userEmail);
-
-        public int resetPassword(MemberVO vo);
-
-        public int createChatRoom(MemberVO vo);
-        public MemberVO findUserByUserIdx(Integer userIdx);
-        public List<MemberVO> member_getClassList(int userClass);
-
-        public List<MemberVO> user_getWithStar(int userClass);
+  @Autowired
+  public List<MemberVO> member_getList();
 
 
-        //별점주기 위해 추가
-        int findRoomIdx(int userIdx);
+  public int user_join(MemberVO vo);
 
-        int findMatchingUserIdx(int roomIdx, int userIdx);
+  public Integer checkId(String userId);
+
+  public MemberVO loginId(String userId);
+
+  public int userModify(MemberVO vo);
+
+  public int userStatus(MemberVO vo);
+
+  public MemberVO findId(String userEmail);
+
+  public Integer check(String userId, String userPwd);
+
+  public MemberVO userGet(String userId, String userEmail);
+
+  public int resetPassword(MemberVO vo);
+
+  public int createChatRoom(MemberVO vo);
+
+  public MemberVO findUserByUserIdx(Integer userIdx);
+
+  public List<MemberVO> member_getClassList(int userClass);
+
+  public List<MemberVO> user_getWithStar(int userClass);
+
+
+  //별점주기 위해 추가
+  int findRoomIdx(int userIdx);
+
+  int findMatchingUserIdx(int roomIdx, int userIdx);
 }
